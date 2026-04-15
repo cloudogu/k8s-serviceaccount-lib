7bc34af4657df7df08424869a092ffaee96d859d
Niklas <niklas.hinze@cloudogu.com>
2026-04-15 09:25:36 +0200

    #1 Add service account request definition

--- api/v1/serviceaccount_request_types.go
+++ api/v1/serviceaccount_request_types.go
@@ -37,7 +37,28 @@ type LocalSecretRef struct {
 	Name string `json:"name"`
 }
 
+// ServiceAccountRequestParams defines the param structure for the service account creation with unordered options as a mapping from map string to a slice from strings.
+// We do not use just string to string because some parameters maybe require multiple values of the same parameter/option (e.g. --repo x --repo y --permissions read).
+// Although some parameters are unnamed and positional, so we need the additional args field for this. It defines positional arguments without names.
 type ServiceAccountRequestParams struct {
+	// +kubebuilder:validation:MaxProperties=20
+	// +optional
+	Options map[string][]string `json:"options,omitempty"`
+
+	// +kubebuilder:validation:MaxItems=50
+	// +optional
+	Args []string `json:"args,omitempty"`
+}
+
+// ServiceAccountRotation defines a plan for recreating the service account credentials. If ServiceAccountRotation.Enabled is false, nothing will happen.
+// If ServiceAccountRotation.Enabled and a cron expression is set with ServiceAccountRotation.Rotation the account will be recreated defined with ServiceAccountRotation.Rotation.
+// +kubebuilder:validation:XValidation:rule="self.enabled == true ? self.rotation.size() > 0 : true",message="rotation must be set if enabled is true"
+type ServiceAccountRotation struct {
+	// +kubebuilder:default=false
+	Enabled bool `json:"enabled"`
+	// +optional
+	// +kubebuilder:validation:Pattern=`^(@(annually|yearly|monthly|weekly|daily|hourly)|(((\d+,)*\d+|(\d+(\/|-)\d+)|\*)\s?){5,6})$`
+	Rotation string `json:"rotation,omitempty"`
 }
 
 // ServiceAccountRequestSpec defines the desired state of ServiceAccountRequest.
@@ -48,35 +69,44 @@ type ServiceAccountRequestSpec struct {
 	// +kubebuilder:example=false
 	Optional bool `json:"optional,omitempty"`
 
-	// Consumer defines the name of the requesting Dogu or Component
+	// Consumer defines the name of the requesting Dogu or Component.
 	// +required
 	// +kubebuilder:validation:MinLength=1
 	// +kubebuilder:validation:MaxLength=253
-	// +kubebuilder:validation:Pattern=^[a-z0-9]([-a-z0-9]*[a-z0-9])?$
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="Consumer is immutable"
 	// +kubebuilder:example=grafana
 	Consumer string `json:"consumer"`
 
 	// ConsumerType defines the type of the requester. This is necessary for restarting the requester if the account is optional and will be created later.
 	// +required
 	// +kubebuilder:validation:Enum=Dogu;Component
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="ConsumerType is immutable"
 	// +kubebuilder:example=Dogu
-	ConsumerType string `json:"consumerType"`
+	ConsumerType ServiceAccountRequestConsumerType `json:"consumerType"`
 
-	// Producer defines the producer who should process the service account request.
+	// Producer defines the producer who should process the service account request. The value has to correspond to the name of the producer resource.
 	// +required
 	// +kubebuilder:validation:MinLength=1
 	// +kubebuilder:validation:MaxLength=253
-	// +kubebuilder:validation:Pattern=^[a-z0-9]([-a-z0-9]*[a-z0-9])?$
-	// +kubebuilder:example=k8s-prometheus
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="Producer is immutable"
+	// +kubebuilder:example=prometheus
 	Producer string `json:"producer"`
 
 	// SecretRef defines the secret where the generated service account will be written to.
 	// If this field is empty, the service account will be written to a secret named like the name of the serviceaccount request resource.
+	// The final secret reference will be written to the status object.
 	// +optional
 	SecretRef *LocalSecretRef `json:"secretRef,omitempty"`
 
 	// Params defines the parameter which should be used when creating the service account.
+	// +optional
 	Params *ServiceAccountRequestParams `json:"params,omitempty"`
+
+	// Rotation defines timings when the service account should be recreated.
+	// +optional
+	Rotation *ServiceAccountRotation `json:"rotation,omitempty"`
 }
 
 // ServiceAccountRequestStatus defines the observed state of ServiceAccountRequest.
@@ -88,16 +118,29 @@ type ServiceAccountRequestStatus struct {
 	// +listMapKey=type
 	// +optional
 	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
+	// LastRotation defines the timestamp of the last rotation.
+	LastRotation metav1.Time `json:"lastRotation,omitempty"`
+
+	// SecretRef defines the actual secret where the service account was written to.
+	SecretRef *LocalSecretRef `json:"secretRef,omitempty"`
 }
 
-// TODO
+// ServiceAccountRequest define the structure of a service account request.
 // +genclient
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:shortName=sare
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Optional",type="bool",JSONPath=".spec.optional",description="Optional nature of the resource"
-// +kubebuilder:printcolumn:name="Consumer",type="string",JSONPath=".spec.consumer",description="Name of the requester"
-
+// +kubebuilder:printcolumn:name="Consumer",type="string",JSONPath=".spec.consumer"
+// +kubebuilder:printcolumn:name="ConsumerType",type="string",JSONPath=".spec.consumerType",description="Type of the requester"
+// +kubebuilder:printcolumn:name="Producer",type="string",JSONPath=".spec.producer",description="Producer of the requester"
+// +kubebuilder:printcolumn:name="Secret",type="string",JSONPath=".status.secretRef.name",description="Actual secret name"
+// +kubebuilder:printcolumn:name="Rotation",type="boolean",JSONPath=".spec.rotation.enabled"
+// +kubebuilder:printcolumn:name="Schedule",type="string",JSONPath=".spec.rotation.rotation"
+// +kubebuilder:printcolumn:name="Last-Run",type="date",JSONPath=".status.lastRotation"
+// +kubebuilder:printcolumn:name="Params",type="string",JSONPath=".spec.params",priority=1
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 // ServiceAccountRequest is the Schema for the serviceaccount request API.
 type ServiceAccountRequest struct {
 	metav1.TypeMeta `json:",inline"`
